52cb9c039523eb7073068db0ed545ac9b822e874
Survari <tristanpieper080803@gmail.com>
2020-11-02 20:20:48 +0100

    Printing errors to extra file.

--- run.bat
+++ run.bat
@@ -1,3 +1,9 @@
 @echo off
 cd static
-node ..\dist\KIRA.js
+node --trace-warnings ..\dist\KIRA.js 2> error_log.txt
+echo.
+echo.
+echo Program exited. Please do not close the window if there are any errors.
+:loop
+pause
+goto loop
