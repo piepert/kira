748b798dfa3720e276e8d0be248c0be7419d35dc
Survari <tristanpieper080803@gmail.com>
2020-11-05 19:25:56 +0100

    Appending errors instead of replacing file.

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 cd static
-node --trace-warnings ..\dist\KIRA.js 2> error_log.txt
+node --trace-warnings ..\dist\KIRA.js 2>> error_log.txt
 echo.
 echo.
 echo Program exited. Please do not close the window if there are any errors.
