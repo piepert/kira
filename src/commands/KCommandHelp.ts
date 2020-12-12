22e8d5eeb855633f38c90e3a7c6cca707c5d1bee
Survari <tristanpieper080803@gmail.com>
2020-12-12 18:06:32 +0100

    Implemented help command.

--- src/commands/KCommandHelp.ts
+++ src/commands/KCommandHelp.ts
@@ -23,6 +23,10 @@ export class KCommandHelp extends KCommand {
         ]
     }
 
+    public validateSyntax(cmd: KParsedCommand): boolean {
+        return cmd.getArguments().length < 2;
+    }
+
     public async run(conf: KConf,
         msg: Message,
         server: KServer,
@@ -35,7 +39,113 @@ export class KCommandHelp extends KCommand {
         //      !help           - show every command (see picture)
         //      !help <command> - detailed description
 
-        msg.channel.send("Wow, such a crazy emptiness! Wow patch-magic!")
-        console.log("HELP COMMAND NOT FINISHED!")
+        let prefix = conf.getConfig().command_prefix;
+        let user = server.getUser(msg.author.id);
+
+        if (command.getArguments().length == 0 ||
+            command.getArguments()[0] == "all") {
+
+            let embed = new MessageEmbed()
+                .setColor("#6fdefc")
+                .setTitle(conf.getTranslationStr(
+                    msg,
+                    "command.help.list.title"));
+
+            for (let c of KCommandManager.commands) {
+                if (user.canPermissionsOR(c.getPermissions()) ||
+                    conf.userIsOperator(user.id) ||
+                    command.getArguments()[0] == "all") {
+
+                    if (c.getPermissions().includes("OPERATOR") &&
+                        !conf.userIsOperator(user.id) ||
+                        server.isCommandDeactivated(c.getName())) {
+
+                        continue;
+                    }
+
+                    let command_help = conf.getTranslationStr(
+                        msg,
+                        "command.help.command."+c.getName());
+
+                    let names = server.getAliasesForCommand(
+                            c.getName());
+
+                    let name = prefix+c.getName()+
+                        (names.length > 0 ? " (aka " : "")+
+                        names.map(e => prefix+e)
+                            .join(", ")+
+                        (names.length > 0 ? ")" : "");
+
+                    if (command_help == undefined) {
+                        embed.addField(name,
+                            conf.getTranslationStr(
+                                msg,
+                                "command.help.no_help_found"));
+                        continue;
+                    }
+
+                    embed.addField(name, command_help
+                        .std
+                        .replace("{p}", prefix));
+                }
+            }
+
+            msg.channel.send(embed);
+            msg.channel.send(conf.getTranslationStr(
+                        msg,
+                        "command.help.list.footer")
+                    .replace("{1}", prefix))
+
+        } else if (command.getArguments().length == 1) {
+            let name = command.getArguments()[0];
+
+            if (server.getAlias(name) != undefined) {
+                name = server.getAlias(name);
+            }
+
+            let c: KCommand = KCommandManager.getCommand(name);
+
+            if (c == undefined) {
+                msg.channel.send(conf.getTranslationStr(
+                    msg,
+                    "command.help.no_help_found"))
+                return;
+
+            } else {
+                let command_help = conf.getTranslationStr(
+                    msg,
+                    "command.help.command."+c.getName());
+
+                if (command_help == undefined ||
+                    command_help.subcommands == undefined) {
+
+                    msg.channel.send(conf.getTranslationStr(
+                        msg,
+                        "command.help.no_help_found"))
+                    return;
+
+                }
+
+                let embed = new MessageEmbed()
+                    .setColor("#6fdefc")
+                    .setTitle(conf.getTranslationStr(
+                        msg,
+                        "command.help.sub.title")
+                            .replace("{1}", prefix)
+                            .replace("{2}", c.getName()));
+
+                for (let help of command_help.subcommands) {
+                    embed.addField(help.syntax
+                        .replace("{p}", prefix)
+                        .replace("{c}", command.getArguments()[0]),
+
+                        help.help
+                            .replace("{p}", prefix)
+                            .replace("{c}", command.getArguments()[0]));
+                }
+
+                msg.channel.send(embed);
+            }
+        }
     }
 }
