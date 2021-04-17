0cc8d6f755f631774d8f36d8d684bad22c3b3537
Survari <tristanpieper080803@gmail.com>
2021-04-17 11:25:28 +0200

    Added unmute command.

---
+++ src/commands/KCommandUnmute.ts
@@ -0,0 +1,54 @@
+import {
+    Message,
+    MessageEmbed,
+    GuildMember,
+    User
+} from "discord.js";
+
+import { KCommand } from "./KCommand";
+import { KConf } from "../KConfig";
+import { KParsedCommand } from "../KParsedCommand";
+import { KServer } from "../KServer";
+import { KUser } from "../KUser";
+import { Client } from "@typeit/discord/Client";
+
+export class KCommandUnmute extends KCommand {
+    constructor() {
+        super()
+        this.command_name = "unmute";
+
+        this.permissions = [
+            "command.unmute"
+        ]
+    }
+
+    public async run(conf: KConf,
+        msg: Message,
+        server: KServer,
+        command: KParsedCommand,
+        sender: KUser,
+        client: Client) {
+
+        const MAX_MUTE_COUNT = 3;
+        let user: GuildMember = msg.guild
+            .members
+            .cache
+            .get(await KConf.userToID(
+                msg.guild,
+                command.getArguments()[0],
+                server));
+
+        let role_id = server.getMuteRoll();
+
+        if (user.roles.cache.has(role_id)) {
+            user.roles.remove(role_id);
+
+            msg.channel.send(server.getTranslation("command.unmute.unmuted")
+                    .replace("{1}", command.getArguments()[0]));
+
+        } else {
+            msg.channel.send(server.getTranslation("command.unmute.not_muted")
+                    .replace("{1}", command.getArguments()[0]));
+        }
+    }
+}
