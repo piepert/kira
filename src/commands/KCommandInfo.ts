775c933c61cf94c98e7904e14771434e937f2ff1
Survari <tristanpieper080803@gmail.com>
2021-04-17 11:25:43 +0200

    Added info command.

---
+++ src/commands/KCommandInfo.ts
@@ -0,0 +1,85 @@
+import {
+    Message,
+    MessageEmbed,
+    GuildMember,
+    User, TextChannel
+} from "discord.js";
+
+import { KCommand } from "./KCommand";
+import { KConf } from "../KConfig";
+import { KParsedCommand } from "../KParsedCommand";
+import { KServer } from "../KServer";
+import { KUser } from "../KUser";
+import { Client } from "@typeit/discord/Client";
+
+function hasVowel(c) {
+    const vowels = "aeyiouàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ";
+
+    for (let v of vowels.split("")) {
+        if (c.split("").includes(v)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+export class KCommandInfo extends KCommand {
+    constructor() {
+        super()
+        this.command_name = "info";
+
+        this.permissions = [
+            "command.info"
+        ]
+    }
+
+    public validateSyntax(cmd: KParsedCommand): boolean {
+        return (cmd.getArguments().length == 0);
+    }
+
+    public async run(conf: KConf,
+        msg: Message,
+        server: KServer,
+        command: KParsedCommand,
+        sender: KUser,
+        client: Client) {
+
+        let sec_num = parseInt(process.uptime().toString(), 10); // don't forget the second param
+        let days   = Math.floor(sec_num / 3600 / 60).toString();
+        let hours   = Math.floor(sec_num / 3600).toString();
+        let minutes = Math.floor((sec_num - (parseInt(hours) * 3600)) / 60).toString();
+        let seconds = (sec_num - (parseInt(hours) * 3600) - (parseInt(minutes) * 60)).toString();
+
+        let time = days+"d "+hours+"h "+minutes+"min "+seconds+"s";
+
+        msg.channel.send(new MessageEmbed()
+            .setTitle(server.getTranslation("command.info.title"))
+            .addFields([
+                {
+                    name: server.getTranslation("command.info.version"),
+                    value: "v"+conf.version,
+                    inline: true
+                },
+                {
+                    name: server.getTranslation("command.info.uptime"),
+                    value: time,
+                    inline: true
+                },
+                {
+                    name: server.getTranslation("command.info.node"),
+                    value: process.version,
+                    inline: true
+                },
+                {
+                    name: "GitHub",
+                    value: "https://github.com/survari/kira"
+                },
+                {
+                    name: server.getTranslation("command.info.bugs_features"),
+                    value: "http://scp-int.wikidot.com/forum/c-2968533/tech-help"
+                }
+            ])
+        );
+    }
+}
